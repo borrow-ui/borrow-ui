552760694243aa26acab56e1c7f79eca2f09d576
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-03-20 15:27:05 +0000

    Moved button style and use UI_PREFIX

--- packages/ui/src/components/button/Button.js
+++ packages/ui/src/components/button/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import 'style/components/button.scss';
+import { UI_PREFIX } from 'config';
 
+import 'style/components/button/button.scss';
 
-const BUTTON_CLASS = 'borrow-ui__button';
+const BUTTON_CLASS = `${UI_PREFIX}__button`;
 const BUTTON_CLASS_POSITIVE = `${BUTTON_CLASS}--positive`;
 
 export function Button({ className, mean, children, ...rest }) {
@@ -13,15 +14,15 @@ export function Button({ className, mean, children, ...rest }) {
         buttonClassName = `${buttonClassName} ${BUTTON_CLASS_POSITIVE}`;
     }
 
-    return <button className={buttonClassName} {...rest}>
-        {children}
-    </button>;
+    return (
+        <button className={buttonClassName} {...rest}>
+            {children}
+        </button>
+    );
 }
 
 Button.propTypes = {
     className: PropTypes.string,
-    mean: PropTypes.oneOf([
-        undefined, 'positive'
-    ]),
+    mean: PropTypes.oneOf(['positive']),
     children: PropTypes.node,
 };
