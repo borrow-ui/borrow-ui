9d0e0b0dba9c2c3fa21c4aebb84b8fa7bff3c170
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-03-20 15:24:57 +0000

    Improved configuration with prettier, eslint, peerDependencies and externals

--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -2,40 +2,50 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: { loader: 'babel-loader', },
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					{ loader: 'style-loader' },
-					{ loader: 'css-loader' },
-					{ loader: 'sass-loader' },
-				],
-			},
-			{
-				test: /\.(png|gif|jpg|svg)$/,
-				use: {
-					loader: 'url-loader',
-					options: { limit: 50000, },
-				},
-			},
-		],
-	},
-	resolve: {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: { loader: 'babel-loader' },
+            },
+            {
+                test: /\.scss$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+            },
+            {
+                test: /\.(png|gif|jpg|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: { limit: 50000 },
+                },
+            },
+        ],
+    },
+    resolve: {
         extensions: ['.scss', '.js', '.json', '.png', '.gif', '.jpg', '.svg'],
-        alias: {
-            style: path.resolve(__dirname, 'src', 'style')
-        }
-	},
-	output: {
-		path: path.resolve(__dirname, 'lib/'),
-		publicPath: '',
-		filename: 'index.js',
-		libraryTarget: 'umd',
-	},
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'lib/'),
+        publicPath: '',
+        filename: 'index.js',
+        libraryTarget: 'umd',
+    },
+    externals: {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+            umd: 'react',
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+            umd: 'react-dom',
+        },
+    },
 };
