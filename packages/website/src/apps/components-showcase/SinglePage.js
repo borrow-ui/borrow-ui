cdd06ccb9b13774056ab4037111b1eab92ed3a54
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-03-27 20:01:12 +0000

    Added Cards showcase

--- packages/website/src/apps/components-showcase/SinglePage.js
+++ packages/website/src/apps/components-showcase/SinglePage.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
-import { Page } from '@borrow-ui/ui/lib';
+import { Block, Page } from '@borrow-ui/ui/lib';
 
 import { Badges } from './single-page/Badges';
 import { BreadcrumbsComponent } from './single-page/BreadcrumbsComponent';
 import { Buttons } from './single-page/Buttons';
 import { Colors } from './single-page/Colors';
 import { Sidebars } from './single-page/Sidebars';
+import { Cards } from './single-page/Cards';
 
 export function SinglePage() {
     return (
-        <Page title="Single Page Components Showcase">
-            <Colors />
-            <Buttons />
-            <Badges />
-            <BreadcrumbsComponent />
-            <Sidebars />
+        <Page title="Single Page Components Showcase" className="color-neutral-white-bg">
+            <Block className="color-white-bg" outstanding={true}>
+                <Colors />
+                <Buttons />
+                <Badges />
+            </Block>
+            <Block className="color-white-bg" outstanding={true}>
+                <BreadcrumbsComponent />
+                <Sidebars />
+            </Block>
+            <Block className="color-white-bg" outstanding={true}>
+                <Cards />
+            </Block>
         </Page>
     );
 }
---
+++ packages/website/src/apps/components-showcase/single-page/Cards.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from 'react';
+
+import { Card, Button, Icon } from '@borrow-ui/ui/lib';
+
+export function Cards() {
+    return (
+        <div className="m-b-20">
+            <h1>Cards</h1>
+            <Card
+                standingHover={true}
+                icon={<Icon name="home" className="color-accent" />}
+                elementsProps={{ sideProps: { className: 'color-accent-bg' }}}
+                title="Card sample"
+                subtitle="Example of a card"
+                description="Card can have content, called description, as well as title and subtitle. Check the hover!"
+                controls={
+                    <Fragment>
+                        <div>Tertiary action</div>
+                        <div>
+                            <Button size="smaller" className="m-r-5">
+                                Secondary
+                            </Button>
+                            <Button
+                                size="smaller"
+                                mean="positive"
+                                onClick={() => window.alert('Clicked')}
+                            >
+                                Click me
+                            </Button>
+                        </div>
+                    </Fragment>
+                }
+            />
+
+            <Card
+                shadowed={false}
+                icon={<Icon name="check" className="color-positive" />}
+                elementsProps={{ sideProps: { className: 'color-positive-bg' }}}
+                title="Success!"
+                subtitle="The operation was successfull."
+                description="Your pipeline run successfully. A message that you can later remove or keep visible. Flat without hover and shadow."
+                controls={
+                    <Fragment>
+                        <span />
+                        <Button size="smaller">Dismiss</Button>
+                    </Fragment>
+                }
+            />
+        </div>
+    );
+}
