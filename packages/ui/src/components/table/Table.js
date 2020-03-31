a3fa2839bbf2f9e5f5d786744f259bb08029465e
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-03-31 08:50:34 +0100

    Added table pagination and status bar

--- packages/ui/src/components/table/Table.js
+++ packages/ui/src/components/table/Table.js
@@ -4,27 +4,56 @@ import PropTypes from 'prop-types';
 import { UI_PREFIX } from 'config';
 
 import { TableWrapper } from './TableWrapper';
+import { TablePagination } from './TablePagination';
+import { TableStatusBar } from './TableStatusBar';
 
 // import 'style/component/table/table.scss';
 
 const TABLE_CONTAINER_CLASS = `${UI_PREFIX}__table__container`;
+const TABLE_STATUS_BAR = `${UI_PREFIX}__table__status-bar`;
 
 const DEFAULT_CONFIG = {
     borderType: 'row',
     verticalAlignment: 'middle',
 };
 
-export function Table({ columns, entries, config, className = '', elementsProps = {}}) {
+const DEFAULT_PAGINATION = {
+    pageSize: 20,
+};
+
+const DEFAULT_STATUS_BAR = {
+    visible: true,
+};
+
+export function Table({
+    columns,
+    entries,
+    config,
+    pagination,
+    statusBar,
+    className = '',
+    elementsProps = {},
+}) {
+    const tableConfig = { ...DEFAULT_CONFIG, ...config };
+    const tablePagination = { ...DEFAULT_PAGINATION, ...pagination };
+    const tableStatusBar = { ...DEFAULT_STATUS_BAR, ...statusBar };
+
+    const totEntries = entries.length;
+
     const [tableState, setTableState] = useState({
         columns: [...columns],
         page: 1,
+        pageSize: tablePagination.pageSize,
     });
-
-    const tableConfig = { ...DEFAULT_CONFIG, ...config };
 
     const { className: containerClassName = '', ...containerProps } =
         elementsProps.containerProps || {};
     const tableContainerClassName = `${TABLE_CONTAINER_CLASS} ${containerClassName || ''}`;
+
+    const startEntry = (tableState.page - 1) * tableState.pageSize;
+    const filteredEntries = !tablePagination.pageSize
+        ? entries
+        : entries.slice(startEntry, startEntry + tableState.pageSize);
 
     // TableWrapper: used to implement fixed header / columns in next update
     return (
@@ -33,9 +62,21 @@ export function Table({ columns, entries, config, className = '', elementsProps 
                 className={className}
                 tableState={tableState}
                 setTableState={setTableState}
-                entries={entries}
+                entries={filteredEntries}
                 tableConfig={tableConfig}
             />
+            {tableStatusBar.visible && (
+                <div className={TABLE_STATUS_BAR}>
+                    <TableStatusBar tableState={tableState} totEntries={totEntries} />
+                    {tablePagination.pageSize !== 0 && (
+                        <TablePagination
+                            tableState={tableState}
+                            setTableState={setTableState}
+                            totEntries={totEntries}
+                        />
+                    )}
+                </div>
+            )}
         </div>
     );
 }
@@ -45,11 +86,18 @@ Table.propTypes = {
         PropTypes.shape({
             prop: PropTypes.string.isRequired,
             title: PropTypes.string,
-            width: PropTypes.number,
+            width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+            padding: PropTypes.number,
         }).isRequired
     ).isRequired,
     entries: PropTypes.array.isRequired,
     config: PropTypes.shape({}),
+    pagination: PropTypes.shape({
+        pageSize: PropTypes.number,
+    }),
+    statusBar: PropTypes.shape({
+        visible: PropTypes.bool,
+    }),
     className: PropTypes.string,
     elementsProps: PropTypes.object,
 };
---
+++ packages/ui/src/components/table/TablePagination.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { UI_PREFIX } from 'config';
+
+import { Icon } from 'components/icon/Icon';
+import { propTypesChildren } from 'utils/types';
+
+const TABLE_PAGINATION_CLASS = `${UI_PREFIX}__table__pagination`;
+const TABLE_PAGINATION_PAGE_CLASS = `${UI_PREFIX}__table__pagination__page`;
+const TABLE_PAGINATION_PAGE_ACTIVE_CLASS = `${UI_PREFIX}__table__pagination__page--active`;
+
+export function TablePagination({ tableState, setTableState, totEntries }) {
+    const tablePaginationClassName = TABLE_PAGINATION_CLASS;
+
+    const { page, pageSize } = tableState;
+    const maxPage = Math.ceil(totEntries / pageSize);
+
+    const setPage = n => setTableState({ ...tableState, page: n });
+    const commonProps = { page, setPage };
+
+    return (
+        <div className={tablePaginationClassName}>
+            <PaginationPage
+                label={<Icon name="first_page" size="small" />}
+                pageNumber={1}
+                {...commonProps}
+            />
+            {page > 2 && <PaginationPage pageNumber={page - 2} {...commonProps} />}
+            {page > 1 && <PaginationPage pageNumber={page - 1} {...commonProps} />}
+            <PaginationPage pageNumber={page} {...commonProps} />
+            {page + 1 <= maxPage && <PaginationPage pageNumber={page + 1} {...commonProps} />}
+            {page + 2 <= maxPage && <PaginationPage pageNumber={page + 2} {...commonProps} />}
+            <PaginationPage
+                label={<Icon name="last_page" size="small" />}
+                pageNumber={maxPage}
+                {...commonProps}
+            />
+        </div>
+    );
+}
+
+TablePagination.propTypes = {
+    tableState: PropTypes.object.isRequired,
+    setTableState: PropTypes.func.isRequired,
+    totEntries: PropTypes.number.isRequired,
+};
+
+function PaginationPage({ label, pageNumber, page, setPage }) {
+    const pageLabel = label || `${pageNumber}`;
+    const activePageClassName =
+        page === pageNumber && !label ? TABLE_PAGINATION_PAGE_ACTIVE_CLASS : '';
+    const paginationControlClassName = `${TABLE_PAGINATION_PAGE_CLASS} ${activePageClassName}`;
+    return (
+        <div className={paginationControlClassName} onClick={() => setPage(pageNumber)}>
+            {pageLabel}
+        </div>
+    );
+}
+
+PaginationPage.propTypes = {
+    label: propTypesChildren,
+    pageNumber: PropTypes.number.isRequired,
+    page: PropTypes.number.isRequired,
+    setPage: PropTypes.func.isRequired,
+};
