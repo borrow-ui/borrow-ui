caf9bb5a9ebee5f4b35bad1b9b5e085de0eb80ad
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-04-02 21:48:56 +0100

    Set font in website

--- packages/website/src/index.js
+++ packages/website/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'typeface-lato';
+
 import './index.scss';
 import App from './App';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
