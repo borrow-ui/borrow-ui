3943608cf277d7419740cbda757d18a3a4ecdcb4
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-04-02 17:51:33 +0100

    Relaxed prop type for NavbarGroup

--- packages/ui/src/components/navbar/NavbarGroup.js
+++ packages/ui/src/components/navbar/NavbarGroup.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import { UI_PREFIX } from 'config';
+import { propTypesChildren } from 'utils/types';
 
 const NAVBAR_GROUP_CLASS = `${UI_PREFIX}__navbar__group`;
 const NAVBAR_GROUP_ITEM_CLASS = `${UI_PREFIX}__navbar__group__item`;
@@ -20,7 +21,9 @@ export function NavbarGroup({ position, elements, toggleSetItem }) {
                 const itemHeader = item.hasOwnProperty('headerLabel') ? item.headerLabel : null;
                 return (
                     <Fragment key={`navbar-header-group-${position}-${index}`}>
-                        {typeof item === 'string' && <span className={NAVBAR_GROUP_ITEM_CLASS}>{item}</span>}
+                        {typeof item === 'string' && (
+                            <span className={NAVBAR_GROUP_ITEM_CLASS}>{item}</span>
+                        )}
                         {itemHeader && (
                             <span
                                 className={`${NAVBAR_GROUP_ITEM_CLASS} ${NAVBAR_GROUP_ITEM_CLICKABLE_CLASS}`}
@@ -41,7 +44,7 @@ const elementType = PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.node,
     PropTypes.shape({
-        headerLabel: PropTypes.string,
+        headerLabel: propTypesChildren,
         name: PropTypes.string,
         bodyItem: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
         showQueryInput: PropTypes.bool,
