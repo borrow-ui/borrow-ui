2c470923152b2d73d1d26111fbd03006deb44fd0
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-04-11 09:45:05 +0100

    Moved 3rd party styles to a dedicated file

--- packages/website/src/App.js
+++ packages/website/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
 import { useLocation, Link, BrowserRouter } from 'react-router-dom';
 
 import { setConfig } from '@borrow-ui/ui/lib';
