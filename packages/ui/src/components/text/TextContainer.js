7407071de10a13cef504a650bba743731517e4ed
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-04-15 08:28:34 +0100

    Added rest spread to TextContainer

--- packages/ui/src/components/text/TextContainer.js
+++ packages/ui/src/components/text/TextContainer.js
@@ -7,11 +7,15 @@ import { propTypesChildren } from 'utils/types';
 const TEXT_CONTAINER_CLASS = `${UI_PREFIX}__text-container`;
 const TEXT_CONTAINER_CENTERED_CLASS = `${UI_PREFIX}__text-container--centered`;
 
-export function TextContainer({ className = '', centered = false, children }) {
+export function TextContainer({ className = '', centered = false, children, ...rest }) {
     const centeredClass = centered ? TEXT_CONTAINER_CENTERED_CLASS : '';
     const textContainerClassName = `${TEXT_CONTAINER_CLASS} ${centeredClass} ${className}`;
 
-    return <div className={textContainerClassName}>{children}</div>;
+    return (
+        <div className={textContainerClassName} {...rest}>
+            {children}
+        </div>
+    );
 }
 
 TextContainer.propTypes = {
