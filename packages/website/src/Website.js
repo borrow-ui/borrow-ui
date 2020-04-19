ee6ccc52ad6fbf7c86bcc48637d8bf5aa0bd34d0
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-04-19 22:46:43 +0100

    Moved main sidebar to MainSidebar

--- packages/website/src/Website.js
+++ packages/website/src/Website.js
@@ -1,30 +1,20 @@
-import React, { Fragment } from 'react';
-import { useLocation } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-import { Sidebar, SidebarContext, SidebarEntry } from '@borrow-ui/ui/lib';
+import { Sidebar, Responsive } from '@borrow-ui/ui';
 
 import { Header } from './scaffold/header/Header';
 import { Routes } from './routes/Routes';
+import { MainSidebar } from 'scaffold/MainSidebar';
 
 export function Website() {
-    const location = useLocation();
-
-    const className = location.pathname === '/' ? 'website__sidebar--home' : '';
-
     return (
         <div className="borrow-ui borrow-ui__navbar--stycky-margin">
             <Sidebar.Provider>
                 <Header />
                 <div style={{ display: 'flex' }}>
-                    <Sidebar
-                        stickyTop={46}
-                        disableTrigger={true}
-                        height={'calc(100vh - 46px)'}
-                        top={sidebarState => <SidebarContent sidebarState={sidebarState} />}
-                        sidebarContext={SidebarContext}
-                        className={className}
-                    ></Sidebar>
+                    <Responsive>
+                        {matches => <MainSidebar isSmallScreen={matches.small} />}
+                    </Responsive>
                     <div
                         className="website__content"
                         style={{ display: 'inline-block', flexGrow: 1 }}
@@ -36,28 +26,3 @@ export function Website() {
         </div>
     );
 }
-
-function SidebarContent({ sidebarState }) {
-    return (
-        <Fragment>
-            <SidebarEntry sidebarState={sidebarState} iconName="home" link="/">
-                Home
-            </SidebarEntry>
-            <SidebarEntry sidebarState={sidebarState} iconName="dashboard" link="/dashboard">
-                Dashboard
-            </SidebarEntry>
-            <SidebarEntry
-                sidebarState={sidebarState}
-                iconName="palette"
-                link="/components-showcase/single-page"
-                id="sidebar-components-showcase"
-            >
-                Style Showcase
-            </SidebarEntry>
-        </Fragment>
-    );
-}
-
-SidebarContent.propTypes = {
-    sidebarState: PropTypes.object.isRequired,
-};
