ecad113e66f99173618eb961f4a170dcb0fa0127
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-04-25 17:16:40 +0100

    Added reverse buttons to showcase

--- packages/website/src/apps/components-showcase/single-page/components/Buttons.js
+++ packages/website/src/apps/components-showcase/single-page/components/Buttons.js
@@ -1,124 +1,116 @@
 import React from 'react';
 
-import { Block, Button } from '@borrow-ui/ui/lib';
+import { Block, Button, Title } from '@borrow-ui/ui/lib';
 
 export function Buttons() {
     return (
         <Block>
+            <Title tag="h3">Regular</Title>
             <div className="flex-start-center flex--wrap m-b-20">
-                <Button onClick={() => window.alert('test')} className="m-r-20 m-b-10">
-                    Regular
-                </Button>
-                <Button
-                    disabled={true}
-                    onClick={() => window.alert('test')}
-                    className="m-r-20 m-b-10"
-                >
+                <Button onClick={() => window.alert('test')}>Regular</Button>
+                <Button disabled={true} onClick={() => window.alert('test')}>
                     Disabled
                 </Button>
-                <Button
-                    mean="primary"
-                    onClick={() => window.alert('primary')}
-                    className="m-r-20 m-b-10"
-                >
+                <Button mean="primary" onClick={() => window.alert('primary')}>
                     Primary
                 </Button>
                 <Button
                     mean="primary"
                     disabled={true}
                     onClick={() => window.alert('primary disabled')}
-                    className="m-r-20 m-b-10"
                 >
                     Disabled
                 </Button>
-                <Button
-                    mean="accent"
-                    onClick={() => window.alert('accent')}
-                    className="m-r-20 m-b-10"
-                >
+                <Button mean="accent" onClick={() => window.alert('accent')}>
                     Accent
                 </Button>
                 <Button
                     mean="accent"
                     disabled={true}
                     onClick={() => window.alert('accent disabled')}
-                    className="m-r-20 m-b-10"
                 >
                     Disabled
                 </Button>
             </div>
             <div className="flex-start-center flex--wrap m-b-20">
-                <Button
-                    mean="positive"
-                    onClick={() => window.alert('positive clicked')}
-                    className="m-r-20 m-b-10"
-                >
+                <Button mean="positive" onClick={() => window.alert('positive clicked')}>
                     Positive
                 </Button>
-                <Button
-                    mean="positive"
-                    disabled={true}
-                    onClick={() => window.alert('positive')}
-                    className="m-r-20 m-b-10"
-                >
+                <Button mean="positive" disabled={true} onClick={() => window.alert('positive')}>
                     Disabled
                 </Button>
-                <Button
-                    mean="warning"
-                    onClick={() => window.alert('warning')}
-                    className="m-r-20 m-b-10"
-                >
+                <Button mean="warning" onClick={() => window.alert('warning')}>
                     Warning
                 </Button>
                 <Button
                     mean="warning"
                     disabled={true}
                     onClick={() => window.alert('warning disabled')}
-                    className="m-r-20 m-b-10"
                 >
                     Disabled
                 </Button>
-                <Button
-                    mean="negative"
-                    onClick={() => window.alert('negative')}
-                    className="m-r-20 m-b-10"
-                >
+                <Button mean="negative" onClick={() => window.alert('negative')}>
                     Negative
                 </Button>
                 <Button
                     mean="negative"
                     disabled={true}
                     onClick={() => window.alert('negative disabled')}
-                    className="m-r-20 m-b-10"
                 >
                     Disabled
                 </Button>
             </div>
+
+            <Title tag="h3">Reverse</Title>
             <div className="flex-start-center flex--wrap m-b-20">
-                <Button mean="primary" className="m-r-20 m-b-10" flat={true}>
+                <Button onClick={() => window.alert('test')} mean="regular-reverse">
+                    Regular
+                </Button>
+                <Button mean="primary-reverse" onClick={() => window.alert('primary-reverse')}>
+                    Primary
+                </Button>
+                <Button mean="accent-reverse" onClick={() => window.alert('accent-reverse')}>
+                    Accent
+                </Button>
+                <Button mean="positive-reverse" onClick={() => window.alert('positive-reverse')}>
+                    Positive
+                </Button>
+                <Button mean="warning-reverse" onClick={() => window.alert('warning-reverse')}>
+                    Warning
+                </Button>
+                <Button mean="negative-reverse" onClick={() => window.alert('negative-reverse')}>
+                    Negative
+                </Button>
+            </div>
+
+            <Title tag="h3">Styles</Title>
+            <div className="flex-start-center flex--wrap m-b-20">
+                <Button mean="primary" flat={true}>
                     Flat
                 </Button>
-                <Button mean="accent" className="m-r-20 m-b-10" icon="dashboard">
+                <Button mean="accent" icon="dashboard">
                     Icon
                 </Button>
             </div>
+
+            <Title tag="h3">Sizes</Title>
             <div className="flex-start-center flex--wrap m-b-20">
-                <Button mean="primary" className="m-r-20 m-b-10" size="huge">
+                <Button mean="primary" size="huge">
                     Huge
                 </Button>
-                <Button mean="primary" className="m-r-20 m-b-10" size="bigger">
+                <Button mean="primary" size="bigger">
                     Bigger
                 </Button>
-                <Button mean="primary" className="m-r-20 m-b-10" size="big">
+                <Button mean="primary" size="big">
                     Big
                 </Button>
-                <Button mean="primary" className="m-r-20 m-b-10" size="normal">
+                <Button mean="primary" size="normal">
                     Normal
                 </Button>
-                <Button mean="primary" className="m-r-20 m-b-10" size="small">
+                <Button mean="primary" size="small">
                     Small
                 </Button>
-                <Button mean="primary" className="m-r-20 m-b-10" size="smaller">
+                <Button mean="primary" size="smaller">
                     Smaller
                 </Button>
             </div>
