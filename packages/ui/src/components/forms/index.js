585d1737240b832ddbb8709da299c5c12ee9b43f
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-05-03 19:47:37 +0100

    Moved styles components into the component folder

--- packages/ui/src/components/forms/index.js
+++ packages/ui/src/components/forms/index.js
@@ -1,15 +1,15 @@
 import { LAYOUTS } from './constants';
 
-import { Checkbox } from './Checkbox';
-import { DatePicker } from './DatePicker';
-import { Dropzone } from './Dropzone/Dropzone';
-import { DropzoneFiles, DropzoneFile } from './Dropzone/DropzoneFiles';
-import { Field, VField, HField } from './Field';
-import { Input } from './Input';
-import { Label } from './Label';
-import { ReactSelect } from './ReactSelect';
-import { Textarea } from './Textarea';
-import { Toggle } from './Toggle';
+import { Checkbox } from './checkbox/Checkbox';
+import { DatePicker } from './date-picker/DatePicker';
+import { Dropzone } from './dropzone/Dropzone';
+import { DropzoneFiles, DropzoneFile } from './dropzone/DropzoneFiles';
+import { Field, VField, HField } from './field/Field';
+import { Input } from './input/Input';
+import { Label } from './label/Label';
+import { ReactSelect } from './react-select/ReactSelect';
+import { Textarea } from './textarea/Textarea';
+import { Toggle } from './toggle/Toggle';
 
 export const Forms = {
     Checkbox,
