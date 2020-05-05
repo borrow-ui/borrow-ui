f70898135c90c966b21e5e8831e4d4a97fae9cb2
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-05-05 12:11:39 +0100

    Added readableContent and  titleVisibleFollowRef

--- packages/ui/src/components/page/PageBody.js
+++ packages/ui/src/components/page/PageBody.js
@@ -8,11 +8,19 @@ import { propTypesChildren } from '../../utils/types';
 
 const PAGE_BODY_CLASS = `${UI_PREFIX}__page__body`;
 const PAGE_BODY_WITH_HEADER_CLASS = `${UI_PREFIX}__page__body--with-page-header`;
+const PAGE_BODY_READABLE_CONTENT_CLASS = `${UI_PREFIX}__page__body__readable-content`;
 
-export function PageBody({ children, withPageHeader, className = '', pageBodyRef, ...rest }) {
-    const bodyClass = `${PAGE_BODY_CLASS} ${
-        withPageHeader ? PAGE_BODY_WITH_HEADER_CLASS : ''
-    } ${className}`;
+export function PageBody({
+    children,
+    withPageHeader,
+    className = '',
+    readableContent = false,
+    pageBodyRef,
+    ...rest
+}) {
+    const bodyClass = `${PAGE_BODY_CLASS} ${withPageHeader ? PAGE_BODY_WITH_HEADER_CLASS : ''} ${
+        readableContent ? PAGE_BODY_READABLE_CONTENT_CLASS : ''
+    }  ${className}`;
 
     return (
         <div className={bodyClass} ref={pageBodyRef} {...rest}>
@@ -25,5 +33,6 @@ PageBody.propTypes = {
     withPageHeader: PropTypes.bool,
     className: PropTypes.string,
     pageBodyRef: PropTypes.object,
+    readableContent: PropTypes.bool,
     children: propTypesChildren,
 };
