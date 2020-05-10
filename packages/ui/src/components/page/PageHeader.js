72c6e7511d8d4a5068c358f492ea30f43a6aa13c
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-05-10 17:06:27 +0100

    Removed Fragment from PageHeader imports

--- packages/ui/src/components/page/PageHeader.js
+++ packages/ui/src/components/page/PageHeader.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { UI_PREFIX } from '../../config';
