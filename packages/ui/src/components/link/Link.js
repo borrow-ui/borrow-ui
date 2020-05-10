c7bc783a91f6a72edb54b0f1708cc7438bdd4718
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-05-10 17:22:50 +0100

    Added underline classes to Link

--- packages/ui/src/components/link/Link.js
+++ packages/ui/src/components/link/Link.js
@@ -5,9 +5,12 @@ import { UI_PREFIX } from '../../config';
 import { propTypesChildren } from '../../utils/types';
 
 const LINK_CLASS = `${UI_PREFIX}__link`;
+const LINK_UNDERLINE_CLASS = `${UI_PREFIX}__link--underline`;
+const LINK_NO_UNDERLINE_CLASS = `${UI_PREFIX}__link--no-underline`;
 
-export function Link({ tag: Tag = 'a', className = '', children, ...rest }) {
-    const linkClassName = `${LINK_CLASS} ${className}`;
+export function Link({ tag: Tag = 'a', className = '', underline = true, children, ...rest }) {
+    const underlineClass = underline ? LINK_UNDERLINE_CLASS : LINK_NO_UNDERLINE_CLASS;
+    const linkClassName = `${LINK_CLASS} ${underlineClass} ${className}`;
 
     return (
         <Tag className={linkClassName} {...rest}>
@@ -19,5 +22,6 @@ export function Link({ tag: Tag = 'a', className = '', children, ...rest }) {
 Link.propTypes = {
     tag: propTypesChildren,
     className: PropTypes.string,
+    underline: PropTypes.bool,
     children: propTypesChildren,
 };
