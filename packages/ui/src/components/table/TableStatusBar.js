6fbc032afa954657d483283a94dbf52b82ee9879
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-05-10 17:45:53 +0100

    Improved Table status bar readability

--- packages/ui/src/components/table/TableStatusBar.js
+++ packages/ui/src/components/table/TableStatusBar.js
@@ -8,7 +8,7 @@ const TABLE_STATUS_BAR_CLASS = `${UI_PREFIX}__table__status-bar`;
 export function TableStatusBar({ tableState, totEntries }) {
     const { page, pageSize } = tableState;
     const firstItem = Math.max(1, (page - 1) * pageSize + 1);
-    const lastItem = Math.min(totEntries, firstItem + pageSize - 1);
+    const lastItem = pageSize ? Math.min(totEntries, firstItem + pageSize - 1) : totEntries;
 
     return (
         <div className={TABLE_STATUS_BAR_CLASS}>
