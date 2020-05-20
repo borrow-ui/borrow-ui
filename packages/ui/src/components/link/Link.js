c07c6b7acc8ba39e1dbf2df6da468967626d4a77
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-05-20 10:16:59 +0100

    Added Link docs and test, and color variables

--- packages/ui/src/components/link/Link.js
+++ packages/ui/src/components/link/Link.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { UI_PREFIX } from '../../config';
+import { UI_PREFIX, getConfig } from '../../config';
 import { propTypesChildren } from '../../utils/types';
 
 const LINK_CLASS = `${UI_PREFIX}__link`;
 const LINK_UNDERLINE_CLASS = `${UI_PREFIX}__link--underline`;
 const LINK_NO_UNDERLINE_CLASS = `${UI_PREFIX}__link--no-underline`;
 
-export function Link({ tag: Tag = 'a', className = '', underline = true, children, ...rest }) {
+export function Link({ tag, className = '', underline = true, children, ...rest }) {
     const underlineClass = underline ? LINK_UNDERLINE_CLASS : LINK_NO_UNDERLINE_CLASS;
     const linkClassName = `${LINK_CLASS} ${underlineClass} ${className}`;
+
+    const Tag = tag || getConfig('getLinkComponent')();
 
     return (
         <Tag className={linkClassName} {...rest}>
