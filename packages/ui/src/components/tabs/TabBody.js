5817696093e3ff91cb3ecf7a6593dee6caf7d90b
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-06-01 18:30:26 +0100

    Added Tabs docs and tests

--- packages/ui/src/components/tabs/TabBody.js
+++ packages/ui/src/components/tabs/TabBody.js
@@ -2,21 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { UI_PREFIX } from '../../config';
+import { propTypesChildren } from '../../utils/types';
 
 const TABS_BODY_CLASS = `${UI_PREFIX}__tabs__body`;
-const TABS_BODY_COMPACT_CLASS = `${UI_PREFIX}__tabs__body--compact`;
+const TABS_BODY_PADDED_CLASS = `${UI_PREFIX}__tabs__body--padded`;
+const TABS_BODY_PADDED_TOP_CLASS = `${UI_PREFIX}__tabs__body--padded-top`;
 const TABS_BODY_SELECTED_CLASS = `${UI_PREFIX}__tabs__body--selected`;
 const TABS_BODY_CONTAINER_CLASS = `${UI_PREFIX}__tabs__body__container`;
 
-export function TabBody({ tabs, selectedTab, compact, className = '', ...rest }) {
-    const compactClassName = compact ? TABS_BODY_COMPACT_CLASS : '';
-
+export function TabBody({
+    tabs,
+    selectedTab,
+    padded = true,
+    paddedTop = false,
+    className = '',
+    ...rest
+}) {
+    const paddedClassName = padded ? TABS_BODY_PADDED_CLASS : '';
+    const paddedTopClassName = paddedTop ? TABS_BODY_PADDED_TOP_CLASS : '';
+    const contentCommonClassName = `${TABS_BODY_CLASS} ${paddedClassName} ${paddedTopClassName}`;
     const containerClass = `${TABS_BODY_CONTAINER_CLASS} ${className}`;
     return (
         <div className={containerClass} {...rest}>
             {tabs.map((tab, index) => {
                 const selectedClassName = index + 1 === selectedTab ? TABS_BODY_SELECTED_CLASS : '';
-                const contentClassName = `${TABS_BODY_CLASS} ${selectedClassName} ${compactClassName}`;
+                const contentClassName = `${contentCommonClassName} ${selectedClassName}`;
 
                 return (
                     <div key={`ui-tabs-body-${index}`} className={contentClassName}>
@@ -31,10 +41,11 @@ export function TabBody({ tabs, selectedTab, compact, className = '', ...rest })
 TabBody.propTypes = {
     tabs: PropTypes.arrayOf(
         PropTypes.shape({
-            content: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+            content: propTypesChildren.isRequired,
         })
     ).isRequired,
     className: PropTypes.string,
     selectedTab: PropTypes.number.isRequired,
-    compact: PropTypes.bool,
+    padded: PropTypes.bool,
+    paddedTop: PropTypes.bool,
 };
