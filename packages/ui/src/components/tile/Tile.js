c246c5aa0bf145c908b3c0028b9cabaa212c5527
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-06-01 18:03:08 +0100

    Added Tile documentation and tests

--- packages/ui/src/components/tile/Tile.js
+++ packages/ui/src/components/tile/Tile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { UI_PREFIX, SIZES } from '../../config';
+import { UI_PREFIX } from '../../config';
 import { propTypesChildren } from '../../utils/types';
 
 const TILE_CLASS = `${UI_PREFIX}__tile`;
@@ -9,9 +9,9 @@ const TILE_WITH_BACKGROUND_CLASS = `${UI_PREFIX}__tile--with-background`;
 const TILE_MAIN_CLASS = `${UI_PREFIX}__tile__main`;
 const TILE_DESCRIPTION_CLASS = `${UI_PREFIX}__tile__description`;
 
-export function Tile({ children, description, size = 'normal', withBackground = false, ...rest }) {
+export function Tile({ children, description, className = '', withBackground = false, ...rest }) {
     const withBackgroundClass = withBackground ? TILE_WITH_BACKGROUND_CLASS : '';
-    const tileClass = `${TILE_CLASS} ${withBackgroundClass}`;
+    const tileClass = `${TILE_CLASS} ${withBackgroundClass} ${className}`;
 
     return (
         <div className={tileClass} {...rest}>
@@ -24,6 +24,6 @@ export function Tile({ children, description, size = 'normal', withBackground = 
 Tile.propTypes = {
     children: propTypesChildren.isRequired,
     description: propTypesChildren.isRequired,
-    size: PropTypes.oneOf(SIZES),
+    className: PropTypes.string,
     withBackground: PropTypes.bool,
 };
