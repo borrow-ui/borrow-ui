3de266e2a036f7d3d0137b9ac4d2b081e6907701
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-06-07 17:20:24 +0100

    Added Textarea docs and tests

--- packages/ui/src/components/forms/textarea/Textarea.js
+++ packages/ui/src/components/forms/textarea/Textarea.js
@@ -7,17 +7,17 @@ const FORM_TEXTAREA_CLASS = `${UI_PREFIX}__form__field__textarea`;
 const FORM_TEXTAREA_DISABLED_CLASS = `${UI_PREFIX}__form__field__textarea--disabled`;
 const FORM_TEXTAREA_INVALID_CLASS = `${UI_PREFIX}__form__field__textarea--invalid`;
 
-export function Textarea({ className, disabled, invalid, ...rest }) {
+export function Textarea({ className = '', disabled, invalid, ...rest }) {
     const disabledClass = disabled ? FORM_TEXTAREA_DISABLED_CLASS : '';
     const invalidClass = invalid ? FORM_TEXTAREA_INVALID_CLASS : '';
 
-    const textareaClassName = `${FORM_TEXTAREA_CLASS} ${disabledClass} ${invalidClass} ${className}`;
+    const textareaClassName = `${FORM_TEXTAREA_CLASS} ${disabledClass} ${invalidClass} ${className}`.trim();
 
     return <textarea className={textareaClassName} disabled={disabled} {...rest} />;
 }
 
 Textarea.propTypes = {
-    className: PropTypes.string,
     disabled: PropTypes.bool,
     invalid: PropTypes.bool,
+    className: PropTypes.string,
 };
