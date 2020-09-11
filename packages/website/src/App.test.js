62a4f22d088a370027a4c2e9c33b87bc817d236e
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-09-11 16:40:51 +0100

    Bumped testing-library/* to latest

--- packages/website/src/App.test.js
+++ packages/website/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders title', () => {
+  const { getAllByText } = render(<App />);
+  const titleElement = getAllByText(/borrow-ui/i);
+  expect(titleElement[0]).toBeInTheDocument();
 });
