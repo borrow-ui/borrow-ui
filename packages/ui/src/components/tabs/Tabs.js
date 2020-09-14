0af3c7f88c1343f501473596e7e18b94dcc45e00
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-09-14 11:26:39 +0100

    Improved Tab documentation

--- packages/ui/src/components/tabs/Tabs.js
+++ packages/ui/src/components/tabs/Tabs.js
@@ -49,6 +49,7 @@ export function Tabs({
 }
 
 Tabs.propTypes = {
+    /** The list of tabs. Each list element is an object with `label` and `content` keys */
     tabs: PropTypes.arrayOf(
         PropTypes.shape({
             label: propTypesChildren.isRequired,
@@ -56,9 +57,14 @@ Tabs.propTypes = {
         })
     ).isRequired,
     className: PropTypes.string,
+    /** Specifies which tab will be open by default (starts from 1) */
     firstOpen: PropTypes.number,
+    /** Applies padding to all body's sides */
     padded: PropTypes.bool,
+    /** Applies padding only on body's top */
     paddedTop: PropTypes.bool,
+    /** Properties to forward to TabHeader */
     tabHeaderProps: PropTypes.object,
+    /** Properties to forward to TabBody */
     tabBodyProps: PropTypes.object,
 };
