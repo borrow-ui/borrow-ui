99ea2bb210112a45f7cb541775169617b64ec3cc
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-09-14 12:50:01 +0100

    Specify Tile tag component

--- packages/ui/src/components/tile/Tile.js
+++ packages/ui/src/components/tile/Tile.js
@@ -9,20 +9,31 @@ const TILE_WITH_BACKGROUND_CLASS = `${UI_PREFIX}__tile--with-background`;
 const TILE_MAIN_CLASS = `${UI_PREFIX}__tile__main`;
 const TILE_DESCRIPTION_CLASS = `${UI_PREFIX}__tile__description`;
 
-export function Tile({ children, description, className = '', withBackground = false, ...rest }) {
+export function Tile({
+    tag: Tag = 'div',
+    children,
+    description,
+    className = '',
+    withBackground = false,
+    ...rest
+}) {
     const withBackgroundClass = withBackground ? TILE_WITH_BACKGROUND_CLASS : '';
     const tileClass = `${TILE_CLASS} ${withBackgroundClass} ${className}`;
 
     return (
-        <div className={tileClass} {...rest}>
+        <Tag className={tileClass} {...rest}>
             <div className={TILE_MAIN_CLASS}>{children}</div>
             <div className={TILE_DESCRIPTION_CLASS}>{description}</div>
-        </div>
+        </Tag>
     );
 }
 
 Tile.propTypes = {
+    /** Customize the tag of Tile */
+    tag: propTypesChildren,
+    /** Tile's content */
     children: propTypesChildren.isRequired,
+    /** Description which will appear on the bottom part of the tile */
     description: propTypesChildren.isRequired,
     className: PropTypes.string,
     withBackground: PropTypes.bool,
