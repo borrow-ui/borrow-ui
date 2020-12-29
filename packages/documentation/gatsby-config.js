c461aa4c61887830cb227ec59c20124908fbcaba
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2020-12-29 18:48:14 +0000

    Removed useless import scss from config

--- packages/documentation/gatsby-config.js
+++ packages/documentation/gatsby-config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  plugins: [
-    "gatsby-plugin-catch-links",
-    {
-      resolve: "gatsby-plugin-sass",
-      options: {
-        data: `@import "${__dirname}/../../ui/src/style/ui.full.scss";`,
-      },
-    },
-  ],
+  plugins: ["gatsby-plugin-catch-links", "gatsby-plugin-sass"],
 };
