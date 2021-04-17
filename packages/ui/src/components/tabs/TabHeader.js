58388d4174f9f09bd144637bd1d3193403d31e3f
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2021-04-17 19:58:17 +0100

    Added Tab extraHeader prop Fixed a11y clickable element in TabHeader

--- packages/ui/src/components/tabs/TabHeader.js
+++ packages/ui/src/components/tabs/TabHeader.js
@@ -8,25 +8,34 @@ import { propTypesChildren } from '../../utils/types';
 const TABS_HEADER_CLASS = `${UI_PREFIX}__tabs__header`;
 const TABS_HEADER_LABEL_CLASS = `${UI_PREFIX}__tabs__header__label`;
 const TABS_HEADER_LABEL_SELECTED_CLASS = `${UI_PREFIX}__tabs__header__label--selected`;
+const TABS_HEADER_EXTRA_HEADER_CLASS = `${UI_PREFIX}__tabs__header__extra-header`;
 
-export function TabHeader({ tabs, selectedTab, setSelectedTab, className = '', ...rest }) {
+export function TabHeader({
+    tabs,
+    selectedTab,
+    setSelectedTab,
+    className = '',
+    extraHeader,
+    ...rest
+}) {
     const tabHeaderClassName = `${TABS_HEADER_CLASS} ${className}`;
-
     return (
         <div className={tabHeaderClassName} {...rest}>
             {tabs.map((tab, index) => {
                 const classSelected =
                     index + 1 === selectedTab ? TABS_HEADER_LABEL_SELECTED_CLASS : '';
+                const onClick = () => setSelectedTab(index + 1);
                 return (
                     <div
                         key={`ui-tabs-header-label-${index}`}
                         className={`${TABS_HEADER_LABEL_CLASS} ${classSelected}`}
-                        {...a11yClickableElement({ onClick: () => setSelectedTab(index + 1) })}
+                        {...a11yClickableElement({ onClick })}
                     >
                         {tab.label}
                     </div>
                 );
             })}
+            {extraHeader && <div className={TABS_HEADER_EXTRA_HEADER_CLASS}>{extraHeader}</div>}
         </div>
     );
 }
@@ -42,4 +51,6 @@ TabHeader.propTypes = {
     selectedTab: PropTypes.number.isRequired,
     /** Function called when tab is selected */
     setSelectedTab: PropTypes.func.isRequired,
+    /** Element to be shown after all labels */
+    extraHeader: propTypesChildren,
 };
