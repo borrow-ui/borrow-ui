4612eaf1c94b31244d2f747ee80b5273f1a85234
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2021-04-28 18:16:15 +0100

    Added custom jest thresholds

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
     },
     setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
     coverageReporters: ['text', 'html'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 90,
+            lines: 90,
+            statements: 80,
+        },
+    },
 };
