269ce4b5b57a661c74e5f024a31c6f17ff866c99
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2021-05-14 17:15:19 +0100

    Test Tile

---
+++ packages/ui/src/components/tile/tile.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { Tile } from './Tile';
+import { UI_PREFIX } from '../../config';
+
+describe('Tile', () => {
+    test('renders with all elements', () => {
+        render(
+            <Tile description="This is the description" data-testid="tile">
+                Content
+            </Tile>
+        );
+
+        expect(screen.getByTestId('tile')).toHaveClass(`${UI_PREFIX}__tile`);
+    });
+    test('renders with backgorund class and custom class', () => {
+        render(
+            <Tile
+                data-testid="tile"
+                description="description"
+                withBackground={true}
+                className="extra-class"
+            >
+                Content
+            </Tile>
+        );
+
+        expect(screen.getByTestId('tile')).toHaveClass(`${UI_PREFIX}__tile`);
+        expect(screen.getByTestId('tile')).toHaveClass(`${UI_PREFIX}__tile--with-background`);
+        expect(screen.getByTestId('tile')).toHaveClass('extra-class');
+    });
+});
