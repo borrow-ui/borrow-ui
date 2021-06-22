44f073889aabcf290cd9bb3c0cc9b92f78578455
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2021-06-22 08:47:48 +0100

    Added SearchBar story about filtering JSX

--- packages/ui/src/components/search-bar/SearchBarStory.js
+++ packages/ui/src/components/search-bar/SearchBarStory.js
@@ -5,26 +5,34 @@ import 'react-day-picker/lib/style.css';
 
 import { propTypesChildren } from '../../utils/types';
 
-export function SearchBarStoryWrapper({ entries, children }) {
+export function SearchBarStoryWrapper({ entries, children, hideStoryOutput = false }) {
     const [filteredEntries, setFilteredEntries] = useState(entries);
     const [globalSearch, setGlobalSearch] = useState(null);
 
     return (
         <>
-            {children({ entries, setFilteredEntries, globalSearch, setGlobalSearch })}
-            <div>
-                <h3>Story filtered entries:</h3>
-                <ul>
-                    {filteredEntries.map((entry) => (
-                        <li key={entry.id}>
-                            {Object.keys(entry)
-                                .map((key) => `${key}: ${entry[key]}`)
-                                .join(', ')}
-                            : {}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            {children({
+                entries,
+                filteredEntries,
+                setFilteredEntries,
+                globalSearch,
+                setGlobalSearch,
+            })}
+            {!hideStoryOutput && (
+                <div>
+                    <h3>Story filtered entries:</h3>
+                    <ul>
+                        {filteredEntries.map((entry) => (
+                            <li key={entry.id}>
+                                {Object.keys(entry)
+                                    .map((key) => `${key}: ${entry[key]}`)
+                                    .join(', ')}
+                                : {}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </>
     );
 }
@@ -32,4 +40,5 @@ export function SearchBarStoryWrapper({ entries, children }) {
 SearchBarStoryWrapper.propTypes = {
     entries: PropTypes.array,
     children: propTypesChildren,
+    hideStoryOutput: PropTypes.bool,
 };
