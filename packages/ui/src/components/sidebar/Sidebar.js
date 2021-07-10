837de3fda86125cd4abfbb248d408573afeda1ed
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2021-07-10 16:25:46 +0100

    Updated sidebar with context Bumped version

--- packages/ui/src/components/sidebar/Sidebar.js
+++ packages/ui/src/components/sidebar/Sidebar.js
@@ -1,10 +1,11 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { Fragment, createContext, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { UI_PREFIX } from '../../config';
-import { a11yClickableElement } from '../../utils/a11y';
 import { propTypesChildren } from '../../utils/types';
-import { Icon } from '../icon/Icon';
+
+import { SidebarEntry, SidebarEntryPropTypes } from './SidebarEntry';
+import { SidebarTrigger, SidebarTriggerPropTypes } from './SidebarTrigger';
 
 const SIDEBAR_CLASS = `${UI_PREFIX}__sidebar`;
 const SIDEBAR_CONTAINER_CLASS = `${UI_PREFIX}__sidebar__container`;
@@ -13,42 +14,62 @@ const SIDEBAR_CONTAINER_CLASS = `${UI_PREFIX}__sidebar__container`;
 // closed => `${UI_PREFIX}__sidebar__container--closed`
 const SIDEBAR_CONTAINER_STICKY_CLASS = `${UI_PREFIX}__sidebar__container--sticky`;
 const SIDEBAR_CONTAINER_OPEN_SHADOW_CLASS = `${UI_PREFIX}__sidebar__container--open--shadow`;
-const SIDEBAR_TRIGGER_CLASS = `${UI_PREFIX}__sidebar__trigger`;
 const SIDEBAR_ELEMENTS_CONTAINER_CLASS = `${UI_PREFIX}__sidebar__elements-container`;
 const SIDEBAR_ELEMENTS_CONTAINER_WITH_TRIGGER_CLASS = `${UI_PREFIX}__sidebar__elements-container--with-trigger`;
-const SIDEBAR_TOP_CLASS = `${UI_PREFIX}__sidebar__top-container`;
-// Status class is generated from SIDEBAR_TOP_CLASS:
-// open => `${UI_PREFIX}__sidebar__top-container--open`
-// closed => `${UI_PREFIX}__sidebar__top-container--closed`
-const SIDEBAR_BOTTOM_CLASS = `${UI_PREFIX}__sidebar__bottom-container`;
 
-export function generateSidebarContextProvider({ initialStatus = 'closed' } = {}) {
-    const SidebarContext = createContext([{}, () => ({})]);
+export function useSidebar({ initialStatus = 'closed' } = {}) {
+    const sidebarContext = createContext([{}, null]);
+    const getDefaultStatus = () => ({
+        status: initialStatus,
+        openedEntrySubContent: {},
+    });
 
-    const SidebarProvider = ({ children }) => {
-        const [state, setState] = useState({ status: initialStatus });
+    const ContextualizedSidebar = (props) => {
+        const contextValue = useState(getDefaultStatus());
 
         return (
-            <SidebarContext.Provider value={[state, setState]}>{children}</SidebarContext.Provider>
+            <sidebarContext.Provider value={contextValue}>
+                <Sidebar sidebarContext={sidebarContext} {...props} />
+            </sidebarContext.Provider>
         );
     };
+    ContextualizedSidebar.propTypes = SidebarPropTypes;
 
-    SidebarProvider.propTypes = {
+    const ContextualizedSidebarEntry = (props) => {
+        return <SidebarEntry sidebarContext={sidebarContext} {...props} />;
+    };
+    ContextualizedSidebarEntry.propTypes = SidebarEntryPropTypes;
+
+    const ContextualizedSidebarTrigger = (props) => {
+        return <SidebarTrigger sidebarContext={sidebarContext} {...props} />;
+    };
+    ContextualizedSidebarTrigger.propTypes = SidebarTriggerPropTypes;
+
+    const CustomTrigger = ({ children }) => {
+        const [contextValue, setContextValue] = useContext(sidebarContext);
+        const toggleStatus = () => {
+            const newStatus = contextValue.status === 'open' ? 'closed' : 'open';
+            setContextValue({ ...contextValue, status: newStatus });
+        };
+        return <Fragment>{children({ toggleStatus })}</Fragment>;
+    };
+    CustomTrigger.propTypes = {
         children: propTypesChildren,
-        initialStatus: PropTypes.oneOf(['open', 'closed']),
     };
 
-    return { SidebarContext, SidebarProvider };
+    return {
+        sidebarContext,
+        getDefaultStatus,
+        SidebarEntry: ContextualizedSidebarEntry,
+        Sidebar: ContextualizedSidebar,
+        SidebarTrigger: ContextualizedSidebarTrigger,
+        CustomTrigger,
+    };
 }
 
-export const { SidebarContext, SidebarProvider } = generateSidebarContextProvider();
-
 export function Sidebar({
-    children,
-    top,
-    bottom,
     initialStatus = 'closed',
-    disableTrigger = false,
+    hideTrigger = false,
     height,
     closedWidth,
     stickyTop,
@@ -56,74 +77,39 @@ export function Sidebar({
     sidebarContext,
     className = '',
     style = {},
+    children,
     ...rest
 }) {
-    const emptyContext = createContext([{}, () => ({})]);
-    const [contextState, setContextState] = useContext(sidebarContext || emptyContext);
-    const [componentState, setComponentState] = useState({ status: initialStatus });
-
-    const [state, setState] = sidebarContext
-        ? [contextState, setContextState]
-        : [componentState, setComponentState];
+    const state = useContext(sidebarContext)[0];
 
     const statusClass = `${SIDEBAR_CONTAINER_CLASS}--${state.status}`;
     const stickyClass = stickyTop !== undefined ? `${SIDEBAR_CONTAINER_STICKY_CLASS}` : '';
     const openShadowClass =
         shadowWhenOpen && state.status === 'open' ? `${SIDEBAR_CONTAINER_OPEN_SHADOW_CLASS}` : '';
     const sidebarContainerClass = `${SIDEBAR_CONTAINER_CLASS} ${statusClass} ${stickyClass} ${openShadowClass} ${className}`.trim();
-    const elementsContainerTriggerClass = !disableTrigger
+    const elementsContainerTriggerClass = !hideTrigger
         ? SIDEBAR_ELEMENTS_CONTAINER_WITH_TRIGGER_CLASS
         : '';
     const elementsContainerClass = `${SIDEBAR_ELEMENTS_CONTAINER_CLASS} ${elementsContainerTriggerClass}`.trim();
-    const sidebarTopStatusClass = `${SIDEBAR_TOP_CLASS}--${state.status}`;
-
-    const sidebarTopClass = `${SIDEBAR_TOP_CLASS} ${sidebarTopStatusClass}`.trim();
 
     const width = closedWidth !== undefined && state.status === 'closed' ? closedWidth : undefined;
     const sidebarContainerStyle = { height, top: stickyTop, width, ...style };
 
-    const onTriggerClick = () => {
-        setState({
-            ...state,
-            status: state.status === 'open' ? 'closed' : 'open',
-        });
-    };
-
     return (
-        <div className={sidebarContainerClass} style={sidebarContainerStyle} {...rest}>
+        <aside className={sidebarContainerClass} style={sidebarContainerStyle} {...rest}>
             <div className={SIDEBAR_CLASS}>
-                {!disableTrigger && (
-                    <div
-                        className={SIDEBAR_TRIGGER_CLASS}
-                        {...a11yClickableElement({ onClick: onTriggerClick })}
-                        data-testid="sidebar-trigger"
-                    >
-                        {state.status === 'open' ? <Icon name="close" /> : <Icon name="menu" />}
-                    </div>
-                )}
-                <div className={elementsContainerClass}>
-                    <div className={sidebarTopClass}>
-                        {top && top(state, setState)}
-                        {children}
-                    </div>
-                    <div className={SIDEBAR_BOTTOM_CLASS}>{bottom && bottom(state, setState)}</div>
-                </div>
+                {!hideTrigger && <SidebarTrigger sidebarContext={sidebarContext} />}
+                <div className={elementsContainerClass}>{children}</div>
             </div>
-        </div>
+        </aside>
     );
 }
 
-Sidebar.propTypes = {
-    /** A function used to generate the top part of the sidebar.
-     * It will take two arguments (`sidebarState`, `setSidebarState`) and should return a valid node. */
-    top: PropTypes.func,
-    /** A function used to generate the bottom part of the sidebar.
-     * It will take two arguments (`sidebarState`, `setSidebarState`) and should return a valid node. */
-    bottom: PropTypes.func,
+export const SidebarPropTypes = {
     /** Initial status of the sidebar. */
     initialStatus: PropTypes.oneOf(['open', 'closed']),
-    /** Disable the generation of the trigger to open/close the sidebar. */
-    disableTrigger: PropTypes.bool,
+    /** Hide the trigger to open/close the sidebar. */
+    hideTrigger: PropTypes.bool,
     /** Make the sidebar sticky to the top. */
     stickyTop: PropTypes.bool,
     /** Overrides the height of the container. */
@@ -134,11 +120,13 @@ Sidebar.propTypes = {
     shadowWhenOpen: PropTypes.bool,
     /** If elements does not require sidebar's state, the content can be passed as children. */
     children: propTypesChildren,
-    /** It is possible to pass the `sidebarContext` if this is created outside
-     * (i.e. to use a trigger inside a page header or a menu bar). */
-    sidebarContext: PropTypes.object,
     className: PropTypes.string,
     style: PropTypes.object,
 };
 
-Sidebar.Provider = SidebarProvider;
+Sidebar.propTypes = {
+    /** It is possible to pass the `sidebarContext` if this is created outside
+     * (i.e. to use a trigger inside a page header or a menu bar). */
+    sidebarContext: PropTypes.object,
+    ...SidebarPropTypes,
+};
--- packages/ui/src/components/sidebar/sidebarEntry.test.js
+++ packages/ui/src/components/sidebar/sidebarEntry.test.js
@@ -1,252 +1,191 @@
-import React, { Fragment } from 'react';
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { config } from '../../config';
-import { Sidebar } from './Sidebar';
-import { SidebarEntry, DETAILS_REQUIRES_ID_AND_SETSIDEBARSTATE } from './SidebarEntry';
+import { useSidebar } from './Sidebar';
+import { CONTENT_REQUIRES_ID } from './SidebarEntry';
 import { UI_PREFIX } from '../../config';
 
 describe('Sidebar', () => {
-    test('renders a Sidebar with entries with details', async () => {
-        render(
-            <Sidebar
-                data-testid="sidebar"
-                top={(sidebarState, setSidebarState) => {
-                    return (
-                        <Fragment>
-                            <SidebarEntry
-                                data-testid="with-details"
-                                sidebarState={sidebarState}
-                                setSidebarState={setSidebarState}
-                                id="with-details"
-                                details={<div>Details</div>}
-                            >
-                                With details
-                            </SidebarEntry>
-                            <SidebarEntry
-                                data-testid="another-details"
-                                sidebarState={sidebarState}
-                                setSidebarState={setSidebarState}
-                                id="another-details"
-                                details={<div>Details</div>}
-                            >
-                                Another details
-                            </SidebarEntry>
-                        </Fragment>
-                    );
-                }}
-            />
+    test('renders a Sidebar with entries with content', async () => {
+        const { Sidebar, SidebarEntry } = useSidebar();
+        const { container } = render(
+            <Sidebar data-testid="sidebar">
+                <SidebarEntry
+                    data-testid="with-content"
+                    id="with-content"
+                    content={<div>Content</div>}
+                    entryClickToggleContent={true}
+                >
+                    With content
+                </SidebarEntry>
+                <SidebarEntry
+                    data-testid="another-content"
+                    id="another-content"
+                    content={<div>Content</div>}
+                    entryClickToggleContent={true}
+                >
+                    Another content
+                </SidebarEntry>
+            </Sidebar>
         );
         const sidebarTrigger = screen.getByTestId('sidebar-trigger');
 
-        expect(screen.getByTestId('with-details')).toHaveClass(`${UI_PREFIX}__sidebar__entry`);
-        expect(screen.getByText('With details')).toHaveClass(`${UI_PREFIX}__sidebar__entry__label`);
+        expect(screen.getByTestId('with-content')).toHaveClass(`${UI_PREFIX}__sidebar__entry`);
+        expect(screen.getByText('With content')).toHaveClass(`${UI_PREFIX}__sidebar__entry__label`);
 
-        // Activate "With details"
-        await userEvent.click(screen.getByText('With details'));
-        expect(screen.getByTestId('with-details')).toHaveClass(
-            `${UI_PREFIX}__sidebar__entry--active`
+        // Expand "With content"
+        await userEvent.click(screen.getByText('With content'));
+        expect(container.querySelector('#with-content__content')).toHaveClass(
+            `${UI_PREFIX}__sidebar__entry__content--visible`
         );
-        expect(screen.getByText('With details')).toHaveClass(
-            `${UI_PREFIX}__sidebar__entry__label--active`
-        );
+
         // Open the sidebar as well
         await userEvent.click(sidebarTrigger);
-        // entry should also have an "open" class
-        expect(screen.getByTestId('with-details')).toHaveClass(
-            `${UI_PREFIX}__sidebar__entry--active-open`
+
+        // Expand "Another content"
+        await userEvent.click(screen.getByText('Another content'));
+        expect(container.querySelector('#another-content__content')).toHaveClass(
+            `${UI_PREFIX}__sidebar__entry__content--visible`
         );
-        // Activate "Another details"
-        await userEvent.click(screen.getByText('Another details'));
-        expect(screen.getByTestId('another-details')).toHaveClass(
-            `${UI_PREFIX}__sidebar__entry--active`
+
+        // "With content" should still be expanded
+        expect(container.querySelector('#with-content__content')).toHaveClass(
+            `${UI_PREFIX}__sidebar__entry__content--visible`
         );
-        expect(screen.getByText('Another details')).toHaveClass(
-            `${UI_PREFIX}__sidebar__entry__label--active`
-        );
-        // This shuold de-activate "With details"
-        expect(screen.getByTestId('with-details')).not.toHaveClass(
-            `${UI_PREFIX}__sidebar__entry--active`
-        );
-        expect(screen.getByText('With details')).not.toHaveClass(
-            `${UI_PREFIX}__sidebar__entry__label--active`
-        );
-        // Closes "Another details"
-        await userEvent.click(screen.getByText('Another details'));
-        expect(screen.getByTestId('another-details')).not.toHaveClass(
-            `${UI_PREFIX}__sidebar__entry--active`
-        );
-        expect(screen.getByText('Another details')).not.toHaveClass(
-            `${UI_PREFIX}__sidebar__entry__label--active`
+
+        // Closes "Another content"
+        await userEvent.click(screen.getByText('Another content'));
+        expect(container.querySelector('#another-content__content')).not.toHaveClass(
+            `${UI_PREFIX}__sidebar__entry__content--visible`
         );
     });
-    test('throws an error if details are passed without id or setSidebarState', () => {
+    test('throws an error if content are passed without id', () => {
         const err = console.error;
         console.error = jest.fn();
+
+        const { Sidebar, SidebarEntry } = useSidebar();
+
         expect(() => {
             render(
-                <Sidebar
-                    top={(sidebarState, setSidebarState) => {
-                        return (
-                            <SidebarEntry sidebarState={sidebarState} details={<div>Details</div>}>
-                                With details
-                            </SidebarEntry>
-                        );
-                    }}
-                />
+                <Sidebar>
+                    <SidebarEntry content={<div>More content</div>}>With content</SidebarEntry>
+                </Sidebar>
             );
-        }).toThrowError(DETAILS_REQUIRES_ID_AND_SETSIDEBARSTATE);
-        expect(() => {
-            render(
-                <Sidebar
-                    top={(sidebarState, setSidebarState) => {
-                        return (
-                            <SidebarEntry
-                                sidebarState={sidebarState}
-                                setSidebarState={setSidebarState}
-                                details={<div>Details</div>}
-                            >
-                                With details
-                            </SidebarEntry>
-                        );
-                    }}
-                />
-            );
-        }).toThrowError(DETAILS_REQUIRES_ID_AND_SETSIDEBARSTATE);
-        expect(() => {
-            render(
-                <Sidebar
-                    top={(sidebarState, setSidebarState) => {
-                        return (
-                            <SidebarEntry
-                                sidebarState={sidebarState}
-                                id="with-details"
-                                details={<div>Details</div>}
-                            >
-                                With details
-                            </SidebarEntry>
-                        );
-                    }}
-                />
-            );
-        }).toThrowError(DETAILS_REQUIRES_ID_AND_SETSIDEBARSTATE);
+        }).toThrowError(CONTENT_REQUIRES_ID);
         console.error = err;
     });
+
     test('preserve entry onClick if provided', async () => {
         const testOnClick = jest.fn();
 
+        const { Sidebar, SidebarEntry } = useSidebar();
+
         render(
-            <Sidebar
-                top={(sidebarState, setSidebarState) => {
-                    return (
-                        <Fragment>
-                            <SidebarEntry
-                                data-testid="with-details"
-                                sidebarState={sidebarState}
-                                setSidebarState={setSidebarState}
-                                id="with-details"
-                                details={<div>Details</div>}
-                                onClick={testOnClick}
-                            >
-                                With details
-                            </SidebarEntry>
-                        </Fragment>
-                    );
-                }}
-            />
+            <Sidebar>
+                <SidebarEntry
+                    data-testid="with-content"
+                    id="with-content"
+                    content={<div>Content</div>}
+                    onClick={testOnClick}
+                >
+                    With content
+                </SidebarEntry>
+            </Sidebar>
         );
-        // Activate "With details"
-        await userEvent.click(screen.getByText('With details'));
+        // Activate "With content"
+        await userEvent.click(screen.getByText('With content'));
         // Should have called onClick as well
         expect(testOnClick).toBeCalledTimes(1);
     });
+
     test('reders with an entry with a shortcut', () => {
+        const { Sidebar, SidebarEntry } = useSidebar();
+
         render(
-            <Sidebar
-                top={(sidebarState) => {
-                    return (
-                        <Fragment>
-                            <SidebarEntry sidebarState={sidebarState} shortcut="SC">
-                                Has a shortcut
-                            </SidebarEntry>
-                        </Fragment>
-                    );
-                }}
-            />
+            <Sidebar>
+                <SidebarEntry shortcut="SC">Has a shortcut</SidebarEntry>
+            </Sidebar>
         );
         expect(screen.getByText('SC')).toHaveClass(`${UI_PREFIX}__sidebar__entry__label__shortcut`);
     });
+
     test('reders entry as with a custom tag', () => {
+        const { Sidebar, SidebarEntry } = useSidebar();
+
         const { container } = render(
-            <Sidebar
-                top={(sidebarState) => {
-                    return (
-                        <Fragment>
-                            <SidebarEntry sidebarState={sidebarState} tag="a">
-                                This is a link
-                            </SidebarEntry>
-                        </Fragment>
-                    );
-                }}
-            />
+            <Sidebar>
+                <SidebarEntry tag="a">This is a link</SidebarEntry>
+            </Sidebar>
         );
         expect(container.querySelector('a')).toHaveClass(`${UI_PREFIX}__sidebar__entry`);
     });
+
+    test('reders a shortcut from a longer label', () => {
+        const { Sidebar, SidebarEntry } = useSidebar();
+
+        render(
+            <Sidebar>
+                <SidebarEntry shortcut="Long Label">This is a link</SidebarEntry>
+            </Sidebar>
+        );
+        expect(screen.queryByText('LL')).toBeInTheDocument();
+    });
+
+    test('reders entry with entryClickToggleContent and onClick', async () => {
+        const onClick = jest.fn();
+
+        const { Sidebar, SidebarEntry } = useSidebar();
+
+        render(
+            <Sidebar>
+                <SidebarEntry
+                    entryClickToggleContent={true}
+                    onClick={onClick}
+                    content="Expand"
+                    id="1"
+                >
+                    Entry
+                </SidebarEntry>
+            </Sidebar>
+        );
+
+        await userEvent.click(screen.getByText('Entry'));
+        expect(screen.getByText('Expand')).toHaveClass(
+            `${UI_PREFIX}__sidebar__entry__content--visible`
+        );
+
+        expect(onClick).toHaveBeenCalledTimes(1);
+    });
+
     test('reders entry with Link', () => {
+        const { Sidebar, SidebarEntry } = useSidebar();
+
         const sidebar = (
-            <Sidebar
-                top={(sidebarState) => {
-                    return (
-                        <Fragment>
-                            <SidebarEntry sidebarState={sidebarState} link="/" aria-label="home">
-                                Home
-                            </SidebarEntry>
-                            <SidebarEntry
-                                sidebarState={sidebarState}
-                                link="/tutorials"
-                                aria-label="tutorials"
-                            >
-                                Tutorials
-                            </SidebarEntry>
-                        </Fragment>
-                    );
-                }}
-            />
+            <Sidebar>
+                <SidebarEntry link="/" aria-label="home">
+                    Home
+                </SidebarEntry>
+                <SidebarEntry link="/tutorials" aria-label="tutorials">
+                    Tutorials
+                </SidebarEntry>
+            </Sidebar>
         );
         const { container } = render(sidebar);
         expect(container.querySelector('a')).toHaveClass(`${UI_PREFIX}__sidebar__entry`);
-
-        // home should be active
-        expect(screen.getByRole('navigation', { name: 'home' })).toHaveClass(
-            `${UI_PREFIX}__sidebar__entry--active`
-        );
-        expect(screen.getByRole('navigation', { name: 'tutorials' })).not.toHaveClass(
-            `${UI_PREFIX}__sidebar__entry--active`
-        );
     });
-    test('activate based on URL', () => {
-        const originalGetLocation = config.getLocation;
-        config.getLocation = () => ({ pathname: '/tutorials' });
+    test('activate based on flag', () => {
+        const { Sidebar, SidebarEntry } = useSidebar();
+
         const sidebar = (
-            <Sidebar
-                top={(sidebarState) => {
-                    return (
-                        <Fragment>
-                            <SidebarEntry sidebarState={sidebarState} link="/" aria-label="home">
-                                Home
-                            </SidebarEntry>
-                            <SidebarEntry
-                                sidebarState={sidebarState}
-                                link="/tutorials"
-                                aria-label="tutorials"
-                            >
-                                Tutorials
-                            </SidebarEntry>
-                        </Fragment>
-                    );
-                }}
-            />
+            <Sidebar>
+                <SidebarEntry link="/" aria-label="home">
+                    Home
+                </SidebarEntry>
+                <SidebarEntry link="/tutorials" aria-label="tutorials" isActive={true}>
+                    Tutorials
+                </SidebarEntry>
+            </Sidebar>
         );
         // If navigation changes to tutorials location, it should be active on /tutorials page
         render(sidebar);
@@ -256,6 +195,5 @@ describe('Sidebar', () => {
         expect(screen.getByRole('navigation', { name: 'tutorials' })).toHaveClass(
             `${UI_PREFIX}__sidebar__entry--active`
         );
-        config.getLocation = originalGetLocation;
     });
 });
