41c8f7077be7180c360c0d3186907912d4c553f6
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2021-08-11 10:41:40 +0100

    Workaround to fix indentation in storybook stories

--- packages/documentation/.storybook/preview.js
+++ packages/documentation/.storybook/preview.js
@@ -1,9 +1,23 @@
 import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 import { themes } from "@storybook/theming";
 
+import prettier from "prettier/standalone";
+import prettierBabel from "prettier/parser-babel";
+
 import "!style-loader!css-loader!sass-loader!./scss-loader.scss";
 
 export const parameters = {
+  /*
+  Workaround to make indentation work, waiting for a proper fix
+  https://github.com/storybookjs/storybook/issues/8078
+  */
+  docs: {
+    transformSource: (input) =>
+      prettier.format(input, {
+        parser: "babel",
+        plugins: [prettierBabel],
+      }),
+  },
   previewTabs: { "storybook/docs/panel": { index: -1 } },
   options: {
     storySort: {
