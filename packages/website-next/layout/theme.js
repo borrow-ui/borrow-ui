bf33adacc3924bb429c38d4d07118930c05e77e5
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2022-06-11 18:27:45 +0100

    ThemeTrigger - use a ref to mount only once and useEffect on SSR

--- packages/website-next/layout/theme.js
+++ packages/website-next/layout/theme.js
@@ -1,13 +1,15 @@
-import { useLayoutEffect, useState, useCallback } from 'react';
+import { useLayoutEffect, useEffect, useState, useCallback, useRef } from 'react';
 
 import { Icon } from '@borrow-ui/ui';
+
+const useClientLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
 export function ThemeTrigger() {
     const { theme, toggleTheme } = useThemes();
     const isDark = theme === 'dark';
 
     const iconClass = `m-r-10 ${isDark ? 'color-neutral-white' : 'color-dark'}`;
-
+    7;
     return (
         <div>
             <Icon
@@ -20,6 +22,7 @@ export function ThemeTrigger() {
 }
 
 function useThemes({ defaultTheme = null, localStorageKey = '__website_theme' } = {}) {
+    const mountedRef = useRef(false);
     const [theme, setTheme] = useState(defaultTheme);
 
     const toggleTheme = useCallback(
@@ -39,9 +42,12 @@ function useThemes({ defaultTheme = null, localStorageKey = '__website_theme' } 
         [setTheme, localStorageKey]
     );
 
-    useLayoutEffect(() => {
+    useClientLayoutEffect(() => {
+        if (mountedRef.current) return;
+
         const savedTheme = localStorage && localStorage.getItem(localStorageKey);
         toggleTheme(savedTheme);
+        mountedRef.current = true;
     }, [localStorageKey, toggleTheme]);
 
     return { theme, setTheme, toggleTheme };
